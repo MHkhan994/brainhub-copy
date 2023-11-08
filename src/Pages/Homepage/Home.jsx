fc4cc69b4b5298606233433f750b48f79ec79604
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-11-08 23:46:59 +0600

    navbar and banner

--- src/Pages/Homepage/Home.jsx
+++ src/Pages/Homepage/Home.jsx
@@ -1,9 +1,12 @@
 import Navbar from "../Shared/Navbar";
+import Banner from "./Banner";
 
 const Home = () => {
     return (
-        <div className="min-h-screen bg-dark">
+        <div className="min-h-screen bg-dark h-[300vh]">
             <Navbar></Navbar>
+            <Banner></Banner>
+            <div className="h-screen z-40 w-full bg-white"></div>
         </div>
     );
 };
--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -8,44 +8,43 @@ const Navbar = () => {
     console.log(routes);
 
     return (
-        <div className="my-container h-[80px] py-1 flex justify-between items-center uppercase font-ibm text-white">
-            <div className='flex gap-16 items-center'>
-                <img src="/logo.svg" className="h-[15px]" alt="" />
-                <ul className='flex gap-6'>
-                    {
-                        routes.map((item, index) => <li key={item.id} className='nav-list relative'>
-                            <span className='hover:text-[#49FFE6] cursor-pointer flex justify-center items-center text-sm font-semibold gap-1'>
-                                {item.name}
-                                <MdOutlineKeyboardArrowDown className='nav-arrow text-lg text-gray-500'></MdOutlineKeyboardArrowDown>
-
-
-                            </span>
-
-                            {/* ============dropdown=========== */}
-                            <ul className={`capitalize grid grid-cols-2 dropdown w-[40vw] min-h-[250px] bg-white text-black absolute top-11 z-10 ${index < 3 ? '-left-48' : '-right-48'} font-mona`}>
-                                <div className='bg-slate-100 p-6 flex flex-col justify-between'>
-                                    <h1 className='text-2xl font-medium'>
-                                        {item.dropTitle}
-                                    </h1>
-                                    <button className="border-b flex items-center gap-2">
-                                        get a proposal
-                                        <BiRightArrowAlt className='text-lg'></BiRightArrowAlt>
-                                    </button>
-                                </div>
-                                <div className='px-6'>
-                                    {
-                                        item.routes.map(route => <li className='border-b py-4 text-sm' key={route}>{route}</li>)
-                                    }
-                                </div>
-                            </ul>
-                        </li>)
-                    }
-                </ul>
+        <div className='fixed top-0 left-0 w-full bg-dark z-40'>
+            <div className="my-container h-[80px] py-1 flex justify-between items-center uppercase font-ibm text-white">
+                <div className='flex gap-16 items-center'>
+                    <img src="/logo.svg" className="h-[15px]" alt="" />
+                    <ul className='flex gap-5'>
+                        {
+                            routes.map((item, index) => <li key={item.id} className='nav-list relative'>
+                                <span className='hover:text-[#49FFE6] cursor-pointer flex justify-center items-center text-sm font-semibold gap-1'>
+                                    {item.name}
+                                    <MdOutlineKeyboardArrowDown className='nav-arrow text-lg text-gray-500'></MdOutlineKeyboardArrowDown>
+                                </span>
+                                {/* ============dropdown=========== */}
+                                <ul className={`capitalize grid grid-cols-2 dropdown w-[40vw] min-h-[250px] bg-white text-black absolute top-11 z-10 ${index < 3 ? '-left-48' : '-right-48'} font-mona`}>
+                                    <div className='bg-slate-100 p-6 flex flex-col justify-between'>
+                                        <h1 className='text-2xl font-medium'>
+                                            {item.dropTitle}
+                                        </h1>
+                                        <button className="border-b flex items-center gap-2">
+                                            get a proposal
+                                            <BiRightArrowAlt className='text-lg'></BiRightArrowAlt>
+                                        </button>
+                                    </div>
+                                    <div className='px-6 py-3 pb-6'>
+                                        {
+                                            item.routes.map(route => <li className='border-b py-3 text-sm' key={route}>{route}</li>)
+                                        }
+                                    </div>
+                                </ul>
+                            </li>)
+                        }
+                    </ul>
+                </div>
+                <button className="btn-pri">
+                    get a proposal
+                    <BiRightArrowAlt className='text-lg'></BiRightArrowAlt>
+                </button>
             </div>
-            <button className="btn-pri">
-                get a proposal
-                <BiRightArrowAlt className='text-lg'></BiRightArrowAlt>
-            </button>
         </div>
     );
 };
